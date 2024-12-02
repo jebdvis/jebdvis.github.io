527a90c53ab029124d1e30194c33b889e35192c2
Jeb Davis <jebemery@gmail.com>
2024-12-02 13:58:51 -0700

    some layout stuff and started filling in projects

--- JS_Final/scripts/portfolio.js
+++ JS_Final/scripts/portfolio.js
@@ -17,8 +17,8 @@ function parseData(data){
     document.getElementById("projects").innerHTML += `<a href ="/JS_Final/projects/${data.projects[i].subdomain}.html">
     <div class = "row project" id = "${data.projects[i].subdomain}">
         <div class = "projimg"><img src = "images/${data.projects[i].mainimg}"></div>
-        <div class = "description"><h2>${data.projects[i].description}</h2><p class = "subtitle">${data.projects[i].subtitle}</p>
-        <p>${data.projects[i].abstract}</p></div></div></a>`;
+        <div class = "name"><h2>${data.projects[i].name}</h2><p class = "subtitle">${data.projects[i].subtitle}</p>
+        <p class = "abstract">${data.projects[i].abstract}</p></div></div></a>`;
     }
 }
 
